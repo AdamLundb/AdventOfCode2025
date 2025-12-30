29795ebb4bec4a7b6fcfbf726a4da76b7a7bba62
Adamstudnt <adaluh171@studentmail.oru.se>
2025-12-30 22:57:14 +0100

    start på part 2

--- day7/day7b.py
+++ day7/day7b.py
@@ -1,7 +1,7 @@
 class Level:
     def __init__(self, lvl, layout_string):
         self.level = lvl
-        self.layout = layout_string
+        self.layout = list(layout_string)
         self.indexes = []
         self.splitters = []
         self.has_caused_split = 0
@@ -14,51 +14,82 @@ class Level:
             if(self.layout[i] == "^"):
                 self.splitters.append(i)
 
+class TimeLine:
+    def __init__(self, list_of_Levels, current_level):
+        self.levels = list_of_Levels
+        self.level = current_level
 
+    def update_TimeLine(self, new_level):
+        self.levels.append(new_level)
+        self.level = self.level + 1
 
-def add_beams_from_previous_level(beam_indexes_from_last_level, line_of_current_level):
+    def return_highest_level(self):
+        return self.levels[self.level]
+    def print_timeline(self):
+        for i in range(len(self.levels)):
+            print(self.levels[i].layout)
+
+def add_beams_from_previous_level(beam_indexes_from_last_level, line_of_current_level, current_TimeLine, timelines):
     new_line = list(line_of_current_level)
-    score = 0
+    print(len(timelines))
+    current_TimeLine.print_timeline()
     for i in range(len(beam_indexes_from_last_level)):
         #print(new_line, beam_indexes_from_last_level[i], i)
         if(new_line[beam_indexes_from_last_level[i]] != "^"):
             new_line[beam_indexes_from_last_level[i]] = 'S'
+            new_time_line = TimeLine(current_TimeLine.levels, current_TimeLine.level)
+            new_time_line.update_TimeLine(Level(current_TimeLine.level + 1,new_line))
+            timelines.append(new_time_line)
         if(new_line[beam_indexes_from_last_level[i]] == "^"):
-            if(beam_indexes_from_last_level[i] == 0):
-                new_line[beam_indexes_from_last_level[i] + 1] = 'S'
-            elif(beam_indexes_from_last_level[i] == len(new_line) - 1):
-                new_line[beam_indexes_from_last_level[i] - 1] = 'S'
-            else:
-                new_line[beam_indexes_from_last_level[i] + 1] = 'S'
-                new_line[beam_indexes_from_last_level[i] - 1] = 'S'
-            score += 1
-    return new_line, score
+            left_line = new_line[beam_indexes_from_last_level[i] + 1] = 'S'
+            new_time_line = TimeLine(current_TimeLine.levels, current_TimeLine.level)
+            new_time_line.update_TimeLine(Level(current_TimeLine.level + 1, left_line))
+            timelines.append(new_time_line)
+            right_line = new_line[beam_indexes_from_last_level[i] - 1] = 'S'
+            new_time_line = TimeLine(current_TimeLine.levels, current_TimeLine.level)
+            new_time_line.update_TimeLine(Level(current_TimeLine.level + 1, right_line))
+            timelines.append(new_time_line)
+    print(len(timelines))
+    return timelines
 
 def print_map(data):
     for i in range(len(data)):
         print(data[i].layout)
 
+def check_if_timelines_cotains_level_of_previous(timelines, index_to_check):
+    for i in range(len(timelines)):
+        if(timelines[i].level == index_to_check):
+            return True
+    return False
+def fetch_timeline_of_current_level(timelines, current_level):
+    for i in range(len(timelines)):
+        if(timelines[i].level == current_level):
+            return i
+
 data = []
+Amount_of_lines = 0
+with open('day7/test.txt', 'r') as file:
+    for line in file:
+        Amount_of_lines += 1
+print(Amount_of_lines)
+
 
 i = 0
-with open('day7/input.txt', 'r') as file:
+trees = []
+with open('day7/test.txt', 'r') as file:
     for line in file:
         if(i == 0):
-            data.append(Level(i, line[:-1]))
+            start_line = list(line[:-1])
+            first_level = Level(0, start_line)
+            level_list = [first_level]
+            trees.append(TimeLine(level_list, i))
         else:
-            splits = 0
-            if(line[-1] == "\n"):
-                updated_line, splits = add_beams_from_previous_level(data[i-1].indexes, line[:-1])
-            else:
-                updated_line, splits = add_beams_from_previous_level(data[i-1].indexes, line)
-            new_level = Level(i, updated_line)
-            new_level.has_caused_split = splits
-            data.append(new_level)
+            while check_if_timelines_cotains_level_of_previous(trees, i - 1) == True:
+                time_line_to_handle = trees.pop(fetch_timeline_of_current_level(trees, i - 1))
+                if(line[-1] == "\n"):
+                    current_line = line[:-1]
+                else:
+                    current_line = line
+                trees = add_beams_from_previous_level(time_line_to_handle.return_highest_level().indexes, current_line, time_line_to_handle, trees)
         i += 1
-
-print_map(data)
-
-score = 0
-for i in range(len(data)):
-    score += data[i].has_caused_split
-print(score)
+print(len(trees))
